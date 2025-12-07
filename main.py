1982aa9b9914e1e285acca14fa55f1aadfccc7a8
Frederik Traeholt Andersen <frederikhta@gmail.com>
2025-12-07 16:46:52 +0100

    add command

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 token = os.environ.get("discordToken")
@@ -15,7 +16,16 @@ class MyClient(discord.Client):
         print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run(token)
+# client = MyClient(intents=intents)
+# client.run(token)
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+bot.run(token)
